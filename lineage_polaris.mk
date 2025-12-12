1a61c86c2e4c9d8650ff4625f759409bc9c2c4fa
Luoxue <luoxue520owo@qq.com>
2025-12-12 20:43:14 +0800

    polaris: Disable OMX service

--- lineage_polaris.mk
+++ lineage_polaris.mk
@@ -4,6 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+TARGET_SUPPORTS_OMX_SERVICE := false
+
 $(call inherit-product, device/xiaomi/polaris/device.mk)
 
 # Inherit some common Lineage stuff.
@@ -17,7 +19,7 @@ PRODUCT_MODEL := Mi MIX 2S
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="polaris-user 8.0.0 OPR1.170623.032 V9.5.19.0.ODGMIFA release-keys" \
-    BuildFingerprint=Xiaomi/polaris/polaris:8.0.0/OPR1.170623.032/V9.5.19.0.ODGMIFA:user/release-keys
+    BuildDesc="polaris-user 10 QKQ1.190828.002 V12.5.1.0.QDGCNXM release-keys" \
+    BuildFingerprint=Xiaomi/polaris/polaris:10/QKQ1.190828.002/V12.5.1.0.QDGCNXM:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
